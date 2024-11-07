e80f2b79998efed2d2b1452aa33856e05c685a6c
Fawaz0ibra <129978738+Fawaz0ibra@users.noreply.github.com>
2024-11-07 10:56:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,89 +1,192 @@
-# app.py
+import streamlit as st # for dl
+import pandas as pd # for data frame
+from langchain_huggingface import HuggingFaceEmbeddings # temp for now in the ht we will change to the IBM embedding
+from langchain.text_splitter import RecursiveCharacterTextSplitter # this is to sipletter fo the markdown df
+from langchain_community.vectorstores import FAISS # VDB
+from ibm_watsonx_ai.foundation_models import Model # IBM API call
+import markdown # so the LLM can have getter understanding to the NHL
 
+st.markdown(
+    """
+    <style>
+    /* Hide the Streamlit top bar (header) */
+    header { 
+        visibility: hidden;
+    }
 
-import streamlit as st
-import pandas as pd
-import os
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from ibm_watsonx_ai.foundation_models import Model
-import markdown
-import getpass
+    /* Set the background image */
+    .stApp {
+        background-image: url("https://raw.githubusercontent.com/salehtwj/Milsan/refs/heads/main/images/Screen Shot 1446-05-04 at 2.18.43 AM.png?raw=true");  
+        background-size: cover;
+        background-position: center;
+    }
+
+    /* Import IBM Plex Sans Arabic from Google Fonts */
+    @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@100;200;300;400;500;600;700&display=swap');
+    
+    /* Set the direction of the entire app to right-to-left (RTL) */
+    .stApp {
+        direction: rtl;
+    }
+
+    /* Change font for the title */
+    .stApp h1 {
+        font-family: 'IBM Plex Sans Arabic', sans-serif;  /* Use IBM Plex Sans Arabic */
+        font-size: 40px;
+        text-align: right;  /* Align title to the right */
+    }
+
+    /* Change font for the text under the title (st.write) */
+    .stApp p {
+        font-family: 'IBM Plex Sans Arabic', sans-serif;  /* Use IBM Plex Sans Arabic */
+        font-size: 18px;
+        text-align: right;  /* Align text to the right */
+    }
+    
+
+    /* Position the logos at the bottom left */
+    .bottom-left-logos {
+        position: fixed;
+        bottom: 20px;
+        left: 20px;
+        z-index: 9999;
+    }
+
+    .bottom-left-logos img {
+        width: 100px;
+        height: auto;
+        margin-right: 15px;
+    }
+
+    /* Position the top left logo */
+    .top-left-logo {
+        position: fixed;
+        top: 20px;
+        left: 20px;
+        z-index: 9999;
+    }
+
+    .top-left-logo img {
+        width: 100px;
+        height: auto;
+    }
+
+    [data-baseweb="slider"] {
+	direction: ltr;
+    }
+	[data-testid="stVerticalBlockBorderWrapper"]{
+
+	background: rgba(255, 255, 255, 0.2);
+	padding: 30px;
+	border-radius: 15px;
+
+    }
+    .custom-text {
+            font-family: 'IBM Plex Sans Arabic', sans-serif;
+            font-size: 44px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+    <div class="bottom-left-logos">
+        <img src="https://github.com/salehtwj/Milsan/blob/main/images/SDAIA_logo-removebg-preview.png?raw=true" alt="SDAIA Logo">
+        <img src="https://github.com/salehtwj/Milsan/blob/main/images/safcsp_logo.png?raw=true" alt="SAFCSP Logo">
+        <img src="https://github.com/salehtwj/Milsan/blob/main/images/tuwaiq_logo_w.png?raw=true/" alt="Tuwaiq Logo">
+    </div>
+
+    <div class="top-left-logo">
+        <img src="https://github.com/salehtwj/Milsan/blob/main/images/ALLaM_logo.png?raw=true" alt="ALLaM Logo">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 # Load Data
 data = {
     "Poem": [
-        """قِفا نَبكِ مِن ذِكرى حَبيبٍ وَعِرفانِ   وَرَسمٍ عَفَت آياتُهُ مُنذُ أَزمانِ  أَتَت حُجَجٌ بَعدي عَلَيها فَأَصبَحَت   كَخَطِّ زَبورٍ في مَصاحِفِ رُهبانِ """
-        ,""" أَعِنّي عَلى بَرقٍ أَراهُ وَميضِ  يُضيءُ حَبِيّاً في شَماريخَ بيضِ   وَيَهدَأُ تاراتٍ سَناهُ وَتارَةً   يَنوءُ كَتَعتابِ الكَسيرِ المَهيضِ """
-        ,""" طَرِبتَ وَهاجَتكَ الظِباءُ السَوارِحُ  غَداةَ غَدَت مِنها سَنيحٌ وَبارِحُ  تَغالَت بِيَ الأَشواقُ حَتّى كَأَنَّما   بِزَندَينِ في جَوفي مِنَ الوَجدِ قادِحُ """
-        ,""" أُعاتِبُ دَهراً لا يَلينُ لِعاتِبِ   وَأَطلُبُ أَمناً مِن صُروفِ النَوائِبِ   وَتوعِدُني الأَيّامُ وَعداً تَغُرُّني   وَأَعلَمُ حَقّاً أَنَّهُ وَعدُ كاذِبِ """
-        ,""" دَهَتني صُروفُ الدَهرِ وَاِنتَشَبَ الغَدرُ  وَمَن ذا الَّذي في الناسِ يَصفو لَهُ الدَهرُ   وَكَم طَرَقَتني نَكبَةٌ بَعدَ نَكبَةٍ   فَفَرَّجتُها عَنّي وَما مَسَّني ضُرُّ"""
-
+         """قِفا نَبكِ مِن ذِكرى حَبيبٍ وَعِرفانِ  وَرَسمٍ عَفَت آياتُهُ مُنذُ أَزمانِ"""
+	,""" أَتَت حُجَجٌ بَعدي عَلَيها فَأَصبَحَت  كَخَطِّ زَبورٍ في مَصاحِفِ رُهبانِ """
+        ,""" أَعِنّي عَلى بَرقٍ أَراهُ وَميضِ  يُضيءُ حَبِيّاً في شَماريخَ بيضِ """
+	,""" وَيَهدَأُ تاراتٍ سَناهُ وَتارَةً   يَنوءُ كَتَعتابِ الكَسيرِ المَهيضِ """
+        ,""" طَرِبتَ وَهاجَتكَ الظِباءُ السَوارِحُ  غَداةَ غَدَت مِنها سَنيحٌ وَبارِحُ """
+,""" تَغالَت بِيَ الأَشواقُ حَتّى كَأَنَّما   بِزَندَينِ في جَوفي مِنَ الوَجدِ قادِحُ """
+        ,""" أُعاتِبُ دَهراً لا يَلينُ لِعاتِبِ   وَأَطلُبُ أَمناً مِن صُروفِ النَوائِبِ """
+	,""" وَتوعِدُني الأَيّامُ وَعداً تَغُرُّني   وَأَعلَمُ حَقّاً أَنَّهُ وَعدُ كاذِبِ """
+        ,""" دَهَتني صُروفُ الدَهرِ وَاِنتَشَبَ الغَدرُ  وَمَن ذا الَّذي في الناسِ يَصفو لَهُ الدَهرُ """
+	,""" وَكَم طَرَقَتني نَكبَةٌ بَعدَ نَكبَةٍ   فَفَرَّجتُها عَنّي وَما مَسَّني ضُرُّ"""
+	
         ,
-
-        """رُبَّ رامٍ مِن بَني ثُعَلٍ   مُتلِجٍ كَفَّيهِ في قُتَرِه   عارِضٍ زَوراءَ مِن نَشمٍ  غَيرُ باناةٍ عَلى وَتَرِه """
-        ,""" نَفِّسوا كَربي وَداوُوا عِلَلي   وَاِبرِزوا لي كُلَّ لَيثٍ بَطَلِ   وَاِنهَلوا مِن حَدِّ سَيفي جُرَعاً   مُرَّةً مِثلَ نَقيعِ الحَنظَلِ """
-        ,""" بَكَرَت تَعذُلُني وَسطَ الحِلالِ   سَفَهاً بِنتُ ثُوَيرِ بنِ هِلالِ   بَكَرَت تَعذُلُني في أَن رَأَت   إِبِلي نَهباً لِشَربٍ وَفِضالِ """
-        ,""" ذادَ عَنى النَومَ هَمٌّ بَعدَ هَمّ   وَمِن الهَمِّ عَناءٌ وَسَقَم   طَرَقَت طَلحَةُ رَحلي بَعدَما  نامَ أَصحابى وَلَيلي لَم أَنَم """
+	
+	"""رُبَّ رامٍ مِن بَني ثُعَلٍ   مُتلِجٍ كَفَّيهِ في قُتَرِه """
+	,""" عارِضٍ زَوراءَ مِن نَشمٍ  غَيرُ باناةٍ عَلى وَتَرِه """
+        ,""" نَفِّسوا كَربي وَداوُوا عِلَلي   وَاِبرِزوا لي كُلَّ لَيثٍ بَطَلِ """
+	,""" وَاِنهَلوا مِن حَدِّ سَيفي جُرَعاً   مُرَّةً مِثلَ نَقيعِ الحَنظَلِ """
+        ,""" بَكَرَت تَعذُلُني وَسطَ الحِلالِ   سَفَهاً بِنتُ ثُوَيرِ بنِ هِلالِ """
+	,""" بَكَرَت تَعذُلُني في أَن رَأَت   إِبِلي نَهباً لِشَربٍ وَفِضالِ """
+        ,""" ذادَ عَنى النَومَ هَمٌّ بَعدَ هَمّ   وَمِن الهَمِّ عَناءٌ وَسَقَم """ 
+	,""" طَرَقَت طَلحَةُ رَحلي بَعدَما  نامَ أَصحابى وَلَيلي لَم أَنَم """
         ,""" سَأَلَتني عَن أُناسٍ هَلَكوا  أَكَلَ الدَهرُ عَلَيهِم وَشَرِب"""
 
         ,
 
-        """أَحارِ بنُ عَمروٍ كَأَنّي خَمِر  وَيَعدو عَلى المَرءِ ما يَأتَمِر  لا وَأَبيكَ اِبنَةَ العامِرِيِّ  لا يَدَّعي القَومُ أَنّي أَفِر """
+        """أَحارِ بنُ عَمروٍ كَأَنّي خَمِر  وَيَعدو عَلى المَرءِ ما يَأتَمِر """ 
+	,"""لا وَأَبيكَ اِبنَةَ العامِرِيِّ  لا يَدَّعي القَومُ أَنّي أَفِر """
         ,""" لَعَمرُكَ ما إِن لَهُ صَخرَةً  لَعَمرُكَ ما إِن لَهُ وَزَر """
-        ,""" أَلَم تُكسَفِ الشَمسُ وَالبَدرُ وَالـ  ـكَواكِبُ لِلجَبَلِ الواجِبِ  لِفَقدِ فَضالَةَ لا تَستَوي الـ  ـفُقودُ وَلا خَلَّةُ الذاهِبِ """
-        ,""" أَجِدّوا النِعالَ لِأَقدامِكُم  أَجِدّوا فَوَيهاً لَكُم جَروَلُ  وَأَبلِغ سَلامانَ إِن جِئتَها  فَلا يَكُ شِبهاً لَها المِغزَلُ """
-        ,""" تُخَبِّرُني بِالنَجاةِ القَطاةُ  وَقَولُ الغُرابِ لَها شاهِدُ  تَقولُ أَلا قَد دَنا نازِحٌ  فِداءٌ لَهُ الطارِفُ التالِدُ"""
+        ,""" أَلَم تُكسَفِ الشَمسُ وَالبَدرُ وَالـ  ـكَواكِبُ لِلجَبَلِ الواجِبِ """
+	,""" لِفَقدِ فَضالَةَ لا تَستَوي الـ  ـفُقودُ وَلا خَلَّةُ الذاهِبِ """
+        ,""" أَجِدّوا النِعالَ لِأَقدامِكُم  أَجِدّوا فَوَيهاً لَكُم جَروَلُ """
+	,""" وَأَبلِغ سَلامانَ إِن جِئتَها  فَلا يَكُ شِبهاً لَها المِغزَلُ """
+	 ,""" تُخَبِّرُني بِالنَجاةِ القَطاةُ  وَقَولُ الغُرابِ لَها شاهِدُ """
+	,""" تَقولُ أَلا قَد دَنا نازِحٌ  فِداءٌ لَهُ الطارِفُ التالِدُ"""
 
         ,
 
-        """لِمَنِ الدِيارُ غَشِيتُها بِسُحامِ  فَعَمايَتَينِ فَهُضبُ ذي أَقدامِ  فَصَفا الأَطيطِ فَصاحَتَينِ فَغاضِرٍ  تَمشي النِعاجُ بِها مَعَ الآرامِ """
-        ,""" هَل غادَرَ الشُعَراءُ مِن مُتَرَدَّمِ  أَم هَل عَرَفتَ الدارَ بَعدَ تَوَهُّمِ  يا دارَ عَبلَةَ بِالجَواءِ تَكَلَّمي  وَعَمي صَباحاً دارَ عَبلَةَ وَاِسلَمي """
-        ,""" أَبَني زَبيبَةَ ما لِمُهرِكُمُ  مُتَخَدِّداً وَبُطونُكُم عُجرُ  أَلَكُم بِآلاءِ الوَشيجِ إِذا  مَرَّ الشِياهُ بِوَقعِهِ خُبرُ """
-        ,""" قِف بِالمَنازِلِ إِن شَجَتكَ رُبوعُها  فَلَعَلَّ عَينَكَ تَستَهِلُّ دُموعُها  وَاِسأَل عَنِ الأَظعانِ أَينَ سَرَت بِها  آباؤُها وَمَتى يَكونُ رُجوعُها"""
+        """لِمَنِ الدِيارُ غَشِيتُها بِسُحامِ  فَعَمايَتَينِ فَهُضبُ ذي أَقدامِ """
+	,""" فَصَفا الأَطيطِ فَصاحَتَينِ فَغاضِرٍ  تَمشي النِعاجُ بِها مَعَ الآرامِ """
+        ,""" هَل غادَرَ الشُعَراءُ مِن مُتَرَدَّمِ  أَم هَل عَرَفتَ الدارَ بَعدَ تَوَهُّمِ """
+	,""" يا دارَ عَبلَةَ بِالجَواءِ تَكَلَّمي  وَعَمي صَباحاً دارَ عَبلَةَ وَاِسلَمي """
+        ,""" أَبَني زَبيبَةَ ما لِمُهرِكُمُ  مُتَخَدِّداً وَبُطونُكُم عُجرُ """
+	,""" أَلَكُم بِآلاءِ الوَشيجِ إِذا  مَرَّ الشِياهُ بِوَقعِهِ خُبرُ """
+        ,""" قِف بِالمَنازِلِ إِن شَجَتكَ رُبوعُها  فَلَعَلَّ عَينَكَ تَستَهِلُّ دُموعُها """
+	,""" وَاِسأَل عَنِ الأَظعانِ أَينَ سَرَت بِها  آباؤُها وَمَتى يَكونُ رُجوعُها"""
 
         ,
 
-        """كَم يُبعِدُ الدَهرُ مَن أَرجو أُقارِبُهُ  عَنّي وَيَبعَثُ شَيطاناً أُحارِبُهُ  فَيا لَهُ مِن زَمانٍ كُلَّما اِنصَرَفَت  صُروفُهُ فَتَكَت فينا عَواقِبُهُ """
-        ,""" لا يَحمِلُ الحِقدَ مَن تَعلو بِهِ الرُتَبُ  وَلا يَنالُ العُلا مَن طَبعُهُ الغَضَبُ  وَمَن يِكُن عَبدَ قَومٍ لا يُخالِفُهُم  إِذا جَفوهُ وَيَستَرضي إِذا عَتَبوا """
-        ,""" لَمّا جَفاني أَخِلّائي وَأَسلَمَني  دَهري وَلحمُ عِظامي اليَومَ يُعتَرَقُ  أَقبَلتُ نَحوَ أَبي قابوسَ أَمدَحُهُ  إِنَّ الثَناءَ لَهُ وَالحَمدُ يَتَّفِقُ """
-        ,""" نفع قليلٌ إذا نادى الصدى أُصلا  وحانَ منه لبرد الماء تَغريد  وودعوني فقالوا ساعة انطلقوا  أودى فأودى النَدى والحزم والجود """
-        ,""" قد أصبح الحبل من أسماء مصروما  بعد ائتلافٍ وحب كان مكتوما  واستبدلت خلة مني وقد علمت  أن لن أبيت بوادي الخسف مذموما"""
+        """كَم يُبعِدُ الدَهرُ مَن أَرجو أُقارِبُهُ  عَنّي وَيَبعَثُ شَيطاناً أُحارِبُهُ """
+	,""" فَيا لَهُ مِن زَمانٍ كُلَّما اِنصَرَفَت  صُروفُهُ فَتَكَت فينا عَواقِبُهُ """
+        ,""" لا يَحمِلُ الحِقدَ مَن تَعلو بِهِ الرُتَبُ  وَلا يَنالُ العُلا مَن طَبعُهُ الغَضَبُ """
+	,""" وَمَن يِكُن عَبدَ قَومٍ لا يُخالِفُهُم  إِذا جَفوهُ وَيَستَرضي إِذا عَتَبوا """
+        ,""" لَمّا جَفاني أَخِلّائي وَأَسلَمَني  دَهري وَلحمُ عِظامي اليَومَ يُعتَرَقُ """
+	,""" أَقبَلتُ نَحوَ أَبي قابوسَ أَمدَحُهُ  إِنَّ الثَناءَ لَهُ وَالحَمدُ يَتَّفِقُ """
+        ,""" نفع قليلٌ إذا نادى الصدى أُصلا  وحانَ منه لبرد الماء تَغريد """
+	,""" وودعوني فقالوا ساعة انطلقوا  أودى فأودى النَدى والحزم والجود """
+        ,""" قد أصبح الحبل من أسماء مصروما  بعد ائتلافٍ وحب كان مكتوما """
+	,""" واستبدلت خلة مني وقد علمت  أن لن أبيت بوادي الخسف مذموما"""
+	    
     ],
     "Sea": [
-        "بحر الطويل", "بحر الطويل", "بحر الطويل", "بحر الطويل", "بحر الطويل",
-        "بحر الرمل","بحر الرمل","بحر الرمل","بحر الرمل","بحر الرمل",
-        "بحر المتقارب", "بحر المتقارب", "بحر المتقارب", "بحر المتقارب", "بحر المتقارب",
-        "بحر الكامل", "بحر الكامل", "بحر الكامل", "بحر الكامل",
-        "بحر البسيط","بحر البسيط","بحر البسيط","بحر البسيط","بحر البسيط"
+        "بحر الطويل","بحر الطويل" ,"بحر الطويل", "بحر الطويل", "بحر الطويل","بحر الطويل","بحر الطويل" ,"بحر الطويل", "بحر الطويل", "بحر الطويل",
+	"بحر الرمل","بحر الرمل","بحر الرمل","بحر الرمل", "بحر الرمل","بحر الرمل","بحر الرمل","بحر الرمل","بحر الرمل",
+        "بحر المتقارب", "بحر المتقارب", "بحر المتقارب", "بحر المتقارب", "بحر المتقارب", "بحر المتقارب", "بحر المتقارب", "بحر المتقارب", "بحر المتقارب",
+        "بحر الكامل", "بحر الكامل", "بحر الكامل", "بحر الكامل", "بحر الكامل", "بحر الكامل", "بحر الكامل", "بحر الكامل",
+        "بحر البسيط","بحر البسيط","بحر البسيط","بحر البسيط","بحر البسيط" ,"بحر البسيط","بحر البسيط","بحر البسيط","بحر البسيط","بحر البسيط"
     ]
 }
 
 # Create DataFrame
 df = pd.DataFrame(data)
 
-# Display the DataFrame
-
-# Streamlit App
-st.title("Arabic Poetry Generation and Analysis")
-
-st.write("This app generates and analyzes Arabic poetry using LangChain and IBM Watson.")
-
-# get API key
-api_key = st.text_input("Enter your API key:")  
-
-# User Input
-query = st.text_input("Enter a query (e.g., generate a poem in a specific meter):")
-threshold = st.slider("Select similarity threshold:", 0.0, 1.0, 0.9)
 
 # Define Functions
 def load_data():
-    # Function to load poems and their seas
     return df
 
 def create_documents(df):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=100000)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=10000000)
     marked_text = []
     for i in range(len(df)):
         poem = df['Poem'].iloc[i]
@@ -94,15 +197,13 @@ def create_documents(df):
 
 def create_embedding(documents):
     embeddings = HuggingFaceEmbeddings(model_name="Omartificial-Intelligence-Space/GATE-AraBert-v0")
-  
     return FAISS.from_documents(documents, embeddings)
 
 def get_credentials():
     return {
         "url": "https://eu-de.ml.cloud.ibm.com",
-        "apikey": api_key,  # Replace with your actual API key
-        "project_id": "11af8977-9294-4e73-a863-b7e37a214840",  # Set your actual project_id here
-        # Alternatively, use "space_id": "your_space_id_here" if applicable
+        "apikey": api_key,  
+        "project_id": "11af8977-9294-4e73-a863-b7e37a214840",
     }
 
 
@@ -150,28 +251,56 @@ the_ofth_prompt = """
 """
 
 def generate_poetry_response(query, threshold, model):
-    results = arabic_VDB.similarity_search_with_score(query, k=4)
+    results = arabic_VDB.similarity_search_with_score(query, k=4) # you can add k this is the number of the rag context 
     context_text = "\n\n".join([doc.page_content for doc, score in results if score > threshold])
-    input_with_rag = the_ofth_prompt + context_text + "\n\n" + "  اجب هنا بناء على هذا الطلب : " + query
-    return model.generate(input_with_rag)['results'][0].get('generated_text')
+    input_with_rag = the_ofth_prompt + context_text + " \n\n " + "  اجب هنا بناء على هذا الطلب : " + query
+    return model.generate(input_with_rag)['results'][0].get('generated_text') , context_text
+
+
+
+
+
+# Streamlit App Start
+st.title("أهلا بكم في ضيافة الشاعر النابغة المِلساني")
+
+st.write("هنا تستطيع سؤال الشاعر ملسان عن أبيات او إنشاء قصائد من بحور متعددة من اختياركم")
+
+# get API key
+api_key = st.text_input("أدخل مفتاح الاستخدام")  
+
+# User Input
+query = st.text_input("أكتب طلبك لإنشاء قصيدة من أحد البحور الشعرية ")
+threshold = st.slider("أختر نسبة التقارب المطلوبة:", 0.0, 1.0, 0.9)
+
 
 # Process Data and Display Results
-if st.button("Run Model"):
-    st.write("Loading and processing documents...")
-    documents = create_documents(df)
-    arabic_VDB = create_embedding(documents)
-
-    model_id = "sdaia/allam-1-13b-instruct"
-    parameters = { "decoding_method": "greedy", "max_new_tokens": 200, "repetition_penalty": 1 }
-    
-    model = Model(
-        model_id = model_id,
-        params = parameters,
-        credentials = get_credentials(),
-        project_id = "11af8977-9294-4e73-a863-b7e37a214840"
-	)
-    response = generate_poetry_response(query, threshold, model)
+if st.button("أطلق العنان"):
     st.write("Generated Poetry:")
-    st.write(response)
+    status = st.empty()
+	
+    status.markdown('<div class="custom-text">يتم الإبداع...</div>', unsafe_allow_html=True)
+    documents = create_documents(df) 
+    arabic_VDB = create_embedding(documents)
+	
+    model_id = "sdaia/allam-1-13b-instruct"
+    parameters = { 
+	"decoding_method": "greedy", 
+	"max_new_tokens": 200, 
+	"repetition_penalty": 1 
+	}
+    model = Model(
+        model_id=model_id,
+        params=parameters,
+        credentials=get_credentials(),
+	project_id ="11af8977-9294-4e73-a863-b7e37a214840",
+    )
+    response , rag = generate_poetry_response(query, threshold, model)
+    status.markdown(f'<div class="custom-text">{response}</div>', unsafe_allow_html=True)
+    st.write("Generated RAG:")
+    st.write(rag)
 
+
+
+
+	
 
